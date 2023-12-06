e46466929dceaafffb6c15d52ff2071a68c65501
António Conselheiro <canudos.sudestino@gmail.com>
2023-12-06 18:54:16 -0300

    incluindo validações no formulário de autenticação

--- src/app/shared/nsec-manager-modal/nsec-manager-modal.component.ts
+++ src/app/shared/nsec-manager-modal/nsec-manager-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { CustomValidator } from '@shared/custom-validator/custom-validator';
 import { ModalableDirective } from '@shared/modal/modalable.directive';
 import { Subject } from 'rxjs';
 
@@ -8,5 +10,23 @@ import { Subject } from 'rxjs';
   styleUrls: ['./nsec-manager-modal.component.scss']
 })
 export class NsecManagerModalComponent extends ModalableDirective<void, void> {
+
   response = new Subject<void>();
+
+  accountForm = this.fb.group({
+    nsec: ['', [
+      Validators.required.bind(this),
+      CustomValidator.nostrSecret
+    ]],
+
+    pin: ['', [
+      Validators.required.bind(this),
+    ]]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    super();
+  }
 }
--- src/app/shared/nsec-manager-modal/nsec-manager-modal.module.ts
+++ src/app/shared/nsec-manager-modal/nsec-manager-modal.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NsecManagerModalComponent } from './nsec-manager-modal.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     NsecManagerModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class NsecManagerModalModule { }
