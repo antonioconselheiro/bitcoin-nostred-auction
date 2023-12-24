938d350ca06ffb20a063d11a5fb3b6517365db1d
António Conselheiro <canudos.sudestino@gmail.com>
2023-12-24 16:48:52 -0300

    criando componente footer no modo autenticado

--- src/app/shared/modal-account-manager/modal-account-manager.component.ts
+++ src/app/shared/modal-account-manager/modal-account-manager.component.ts
@@ -1,23 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IProfile } from '@domain/profile.interface';
 import { MainErrorObservable } from '@shared/error/main-error.observable';
 import { ModalableDirective } from '@shared/modal/modalable.directive';
-import { Subject } from 'rxjs';
+import { AuthenticatedProfileObservable } from '@shared/profile-service/authenticated-profile.observable';
+import { Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'auc-modal-account-manager',
   templateUrl: './modal-account-manager.component.html',
   styleUrls: ['./modal-account-manager.component.scss']
 })
-export class ModalAccountManagerComponent extends ModalableDirective<void, boolean> {
+export class ModalAccountManagerComponent
+  extends ModalableDirective<void, boolean>
+  implements OnInit, OnDestroy {
 
+  private subscriptions = new Subscription();
   response = new Subject<boolean | void>();
+
+  profile: IProfile | null = null;
 
   constructor(
     private router: Router,
+    private authenticatedProfile$: AuthenticatedProfileObservable,
     private error$: MainErrorObservable
   ) {
     super();
+  }
+
+  ngOnInit(): void {
+    this.loadProfile();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private loadProfile(): void {
+    this.subscriptions.add(this.authenticatedProfile$.subscribe({
+      next: profile => this.profile = profile,
+      error: error => this.error$.next(error)
+    }));
   }
 
   chooseWrite(): void {
